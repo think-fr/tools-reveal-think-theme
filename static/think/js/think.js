41eab0801f551a643eb4e77abb12f25e086ab80a
valvin <git@valvin.fr>
2019-11-08 16:00:21 +0100

    feat(): start of new design

---
+++ static/think/js/think.js
@@ -0,0 +1,42 @@
+// trick to detect reverse and apply it
+function manipulateHeader() {
+
+    let currentSlide = document.getElementsByClassName('present').item(0);
+    if (currentSlide.classList.contains('reverse') || currentSlide.classList.contains('title')) {
+        console.log('switching slide header');
+        document.getElementById('slide-header').className = "reverse";
+    }
+    else if (currentSlide.classList.contains('think')) {
+        console.log('hidding slide header');
+        document.getElementById('slide-header').className = "hidden";
+    } else {
+        document.getElementById('slide-header').className = '';
+    }
+}
+function manipulateFooter(){
+    let currentSlide = document.getElementsByClassName('present').item(0);
+    if (currentSlide.classList.contains('title')) {
+        document.getElementById('slide-footer').className = '';
+    }
+    else {
+        console.log('hidding slide footer');
+        document.getElementById('slide-footer').className = "hidden";
+    } 
+}
+
+// in case we miss ready event
+setTimeout(function(){
+    manipulateHeader();
+    manipulateFooter();
+
+}, 500)
+Reveal.addEventListener('ready', function (event) {
+    console.log('reveal is ready');
+    manipulateHeader();
+    manipulateFooter();
+});
+
+Reveal.addEventListener('slidechanged', function (event) {
+    manipulateHeader();
+    manipulateFooter();
+});
